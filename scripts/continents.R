65a63c327cc471a9c98b8a74c54ca32380ec4a89
Shane Mackay <shane.mackay@jcu.edu.au>
2015-10-02 14:15:50 +1000

    Create continents plot script

---
+++ scripts/continents.R
@@ -0,0 +1,32 @@
+
+#!/usr/bin/env Rscript
+# R Plotting continents
+
+#check that ggplot2 packages are installed if not
+#install.packages(ggplot2)
+
+library(ggplot2)
+
+#assume you have the gapminder.RData 
+load("data/gapminder.RData")
+
+#creat a function to plot dots per contenent
+#receives a data frame input and makes a plot
+
+draw_dots_continent <- function(df){
+  
+  p <- ggplot(data=df, aes(x=year, y=lifeExp, color=country)) + 
+        geom_point(aes(color=continent)) +
+        ggtitle("Life expectancy by year") + 
+        facet_grid(.~ continent)
+  print(p)
+}
+
+make_pngplot <- function(name){
+  png(file=paste(name,".png",sep=""))
+  draw_dots_continent(gapminder)
+}
+
+#how to retrive input from user
+arg <- commandArgs(TRUE)
+try(make_pngplot(arg[1]))
